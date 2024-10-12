49044490aab5153c86d2aca640227dc961c66db9
Dev713 <53387865+erikg713@users.noreply.github.com>
2024-10-12 12:22:22 -0500

    Update config.py

--- backend/app/config.py
+++ backend/app/config.py
@@ -1,6 +1,7 @@
 import os
 
 class Config:
-    SECRET_KEY = os.environ.get('SECRET_KEY', '')
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL', 'postgresql://user:password@localhost/dbname')
+    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL', 'postgresql://username:password@localhost/palace_of_goods')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+    JWT_SECRET_KEY = os.getenv('JWT_SECRET_KEY', 'your_jwt_secret_key')
+
