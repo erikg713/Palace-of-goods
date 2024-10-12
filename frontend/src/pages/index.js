9b55b05ee6bb227cb0dd661eee94888ddc89cbb9
Dev713 <53387865+erikg713@users.noreply.github.com>
2024-10-12 12:30:59 -0500

    Update pages

---
+++ frontend/src/pages/index.js
@@ -0,0 +1,15 @@
+import Head from 'next/head';
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>Palace of Goods</title>
+      </Head>
+      <main>
+        <h1>Welcome to Palace of Goods</h1>
+        <p>Buy and sell goods securely!</p>
+      </main>
+    </div>
+  );
+}
