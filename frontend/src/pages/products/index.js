4de75458dae17a7d4fb495b20014524550f5210e
Dev713 <53387865+erikg713@users.noreply.github.com>
2024-10-12 20:26:53 -0500

    Update pages

---
+++ frontend/src/pages/products/index.js
@@ -0,0 +1,24 @@
+import { useEffect, useState } from 'react';
+
+export default function Products() {
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    fetch('/api/products')
+      .then(res => res.json())
+      .then(data => setProducts(data));
+  }, []);
+
+  return (
+    <div>
+      <h2>Products</h2>
+      <ul>
+        {products.map(product => (
+          <li key={product.id}>
+            {product.name} - ${product.price}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
