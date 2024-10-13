d5ea94171f743c29ca2e658c91946bd6d0297922
Dev713 <53387865+erikg713@users.noreply.github.com>
2024-10-13 18:36:55 -0500

    Update index.js

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import ReactDOM from 'react-dom/client';
 import './index.css';
+import App from './App';
+import 'bootstrap/dist/css/bootstrap.min.css';  // Import Bootstrap CSS
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<App />);
