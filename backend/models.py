5246a5023df5042e7de02d2e8f9065b00953c4db
Dev713 <53387865+erikg713@users.noreply.github.com>
2024-10-15 15:28:49 -0500

    Create models.py

---
+++ backend/models.py
@@ -0,0 +1,8 @@
+from flask_mongoengine import MongoEngine
+
+db = MongoEngine()
+
+class Product(db.Document):
+    name = db.StringField(required=True)
+    price = db.FloatField(required=True)
+    description = db.StringField()
