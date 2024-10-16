b2f240c92113c9a3053893fdf358a88418bb257e
Dev713 <53387865+erikg713@users.noreply.github.com>
2024-10-16 04:05:05 -0500

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,16 +1,14 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './components/Home';
-import Profile from './components/Profile';
-import Header from './components/Header';
+import Products from './components/Products';
 
 function App() {
   return (
     <Router>
-      <Header />
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/profile" element={<Profile />} />
+        <Route path="/products" element={<Products />} />
       </Routes>
     </Router>
   );
