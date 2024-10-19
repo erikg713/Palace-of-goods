a3ff6bd2fd4ad9ec6e60578d49ab42b79b59ebde
Dev713 <53387865+erikg713@users.noreply.github.com>
2024-10-19 17:34:24 -0500

    Update Home.js

--- frontend/src/components/Home.js
+++ frontend/src/components/Home.js
@@ -1,1 +1,13 @@
+import React from 'react';
+
+const Home = () => {
+  return (
+    <div>
+      <h1>Welcome to the Palace of Goods</h1>
+      <p>Explore our marketplace for amazing products!</p>
+    </div>
+  );
+};
+
+export default Home;
 
