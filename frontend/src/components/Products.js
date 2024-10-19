0d3beb8dadb3d768917501a665cf16452023f5df
Dev713 <53387865+erikg713@users.noreply.github.com>
2024-10-19 17:34:59 -0500

    Update Products.js

--- frontend/src/components/Products.js
+++ frontend/src/components/Products.js
@@ -1,1 +1,30 @@
+import React, { useEffect, useState } from 'react';
+import { fetchProducts } from '../api';
+
+const Products = () => {
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    const loadProducts = async () => {
+      const productsData = await fetchProducts();
+      setProducts(productsData);
+    };
+    loadProducts();
+  }, []);
+
+  return (
+    <div>
+      <h1>Products</h1>
+      <ul>
+        {products.map(product => (
+          <li key={product.id}>
+            {product.name} - ${product.price}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default Products;
 
