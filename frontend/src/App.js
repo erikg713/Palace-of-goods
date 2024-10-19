77f66d76880970ff6d366853f6637cb8b46341f3
Dev713 <53387865+erikg713@users.noreply.github.com>
2024-10-19 17:33:56 -0500

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,1 +1,20 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Home from './components/Home';
+import Products from './components/Products';
+
+function App() {
+  return (
+    <Router>
+      <div>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/products" element={<Products />} />
+        </Routes>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
 
