8ecf78052b629ceedd4852e983daa151ae7a08b3
Dev713 <53387865+erikg713@users.noreply.github.com>
2024-10-24 21:49:00 -0500

    Update app.py

--- backend/app.py
+++ backend/app.py
@@ -62,3 +62,54 @@ Python
 header = {
     'Authorization': "Key YOUR-API-KEY"
 }
+Python
+@app.route('/payment/approve', methods=['POST'])
+def approve():
+    # Build the header for user authentication
+    accessToken = request.form.get('accessToken')
+    userheader = {
+        'Authorization': f"Bearer {accessToken}"
+    }
+    paymentId = request.form.get('paymentId')
+    approveurl = f"https://api.minepi.com/v2/payments/{paymentId}/approve"
+    response = requests.post(approveurl, headers=header)
+    userurl = "https://api.minepi.com/v2/me"
+    userresponse = requests.get(userurl, headers=userheader)
+    userjson = json.loads(userresponse.text)
+    return(response.text)
+
+@app.route('/payment/complete', methods=['POST'])
+def complete():   
+    # Build the header for user authentication
+    accessToken = request.form.get('accessToken')
+    userheader = {
+        'Authorization': f"Bearer {accessToken}"
+    }
+    paymentId = request.form.get('paymentId')
+    txid = request.form.get('txid')
+    userurl = "https://api.minepi.com/v2/me"
+    userresponse = requests.get(userurl, headers=userheader)
+    data = {'txid': txid}
+    approveurl = f"https://api.minepi.com/v2/payments/{paymentId}/complete"
+    response = requests.post(approveurl, headers=header, data=data)
+    return(response.text)
+
+@app.route('/payment/cancel', methods=['POST'])
+def cancel():    
+    paymentId = request.form.get('paymentId')
+    approveurl = f"https://api.minepi.com/v2/payments/{paymentId}/cancel"
+    response = requests.post(approveurl, headers=header)
+    return(response.text)
+
+@app.route('/payment/error', methods=['POST'])
+def error():    
+    paymentId = request.form.get('paymentId')
+    approveurl = f"https://api.minepi.com/v2/payments/{paymentId}/cancel"
+    response = requests.post(approveurl, headers=header)
+    return(response.text)
+
+@app.route('/me', methods=['POST'])
+def getme():
+    userurl = "https://api.minepi.com/v2/me"
+    response = requests.post(userurl, headers=header)
+    return(response.text)
