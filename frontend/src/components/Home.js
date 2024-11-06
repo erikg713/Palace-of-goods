7832a568348fd2fa1e0441ea9daca245348a8d37
Dev713 <53387865+erikg713@users.noreply.github.com>
2024-11-05 18:50:50 -0600

    Update Home.js
    Signed-off-by: Dev713 <53387865+erikg713@users.noreply.github.com>

--- frontend/src/components/Home.js
+++ frontend/src/components/Home.js
@@ -1,13 +1,14 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Home = () => {
   return (
     <div>
-      <h1>Welcome to the Palace of Goods</h1>
-      <p>Explore our marketplace for amazing products!</p>
+      <h1>Welcome to Palace of Goods</h1>
+      <p>Discover and shop amazing products in the Pi Network marketplace!</p>
+      <Link to="/products">Start Shopping</Link>
     </div>
   );
 };
 
 export default Home;
-
