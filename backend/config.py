a7a72f81c73bf688bf7b730c609fb147e4163440
Dev713 <53387865+erikg713@users.noreply.github.com>
2024-11-09 00:44:09 -0600

    Update config.py
    Signed-off-by: Dev713 <53387865+erikg713@users.noreply.github.com>

--- backend/config.py
+++ backend/config.py
@@ -1,13 +1,11 @@
 import os
+from dotenv import load_dotenv
+
+load_dotenv()  # Load environment variables from .env
 
 class Config:
-    SECRET_KEY = os.getenv('SECRET_KEY', 'default_secret_key')
-    JWT_SECRET_KEY = os.getenv('JWT_SECRET_KEY', 'default_jwt_secret_key')
-    DATABASE_URL = os.getenv('DATABASE_URL', 'mongodb://localhost:27017/palace_of_goods')
-
-class DevelopmentConfig(Config):
-    DEBUG = True
-
-class ProductionConfig(Config):
-    DEBUG = False
-
+    SECRET_KEY = os.getenv("SECRET_KEY")
+    SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL")
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    JWT_SECRET_KEY = os.getenv("JWT_SECRET_KEY")
+    CORS_HEADERS = 'Content-Type'
