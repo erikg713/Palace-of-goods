bdb6dd0af998863b542eeb217ccd6be5f10243c4
Dev713 <53387865+erikg713@users.noreply.github.com>
2024-11-09 04:50:34 -0600

    Update App.js
    Signed-off-by: Dev713 <53387865+erikg713@users.noreply.github.com>

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -20,3 +20,16 @@ function App() {
 }
 
 export default App;
+
+import React from 'react';
+import Marketplace from './components/Marketplace';
+
+function App() {
+  return (
+    <div className="App">
+      <Marketplace />
+    </div>
+  );
+}
+
+export default App;
