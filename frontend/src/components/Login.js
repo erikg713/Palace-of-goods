433ffc84f4da017f422b047dd63407d8d07212c4
Dev713 <53387865+erikg713@users.noreply.github.com>
2024-11-09 01:00:36 -0600

    Create Login.js
    Signed-off-by: Dev713 <53387865+erikg713@users.noreply.github.com>

---
+++ frontend/src/components/Login.js
@@ -0,0 +1,23 @@
+import React, { useState } from 'react';
+import { login } from '../services/api';
+
+function Login() {
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        const response = await login({ username, password });
+        localStorage.setItem("token", response.data.access_token);
+    };
+
+    return (
+        <form onSubmit={handleSubmit}>
+            <input type="text" value={username} onChange={(e) => setUsername(e.target.value)} placeholder="Username" />
+            <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} placeholder="Password" />
+            <button type="submit">Login</button>
+        </form>
+    );
+}
+
+export default Login;
