c48dbea5ff986fa1ffeef4ec744350a3a43ede9f
Dev713 <53387865+erikg713@users.noreply.github.com>
2024-11-12 14:45:39 -0600

    Update App.js
    Signed-off-by: Dev713 <53387865+erikg713@users.noreply.github.com>

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -1,1 +1,13 @@
+import React from 'react';
+import Payment from './components/Payment';
 
+function App() {
+  return (
+    <div>
+      <h1>Palace of Goods</h1>
+      <Payment />
+    </div>
+  );
+}
+
+export default App;
