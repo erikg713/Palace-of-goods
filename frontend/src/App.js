ff9d6cfbbe7f442ab8b7ca6936f8a8a35dc11da6
Dev713 <53387865+erikg713@users.noreply.github.com>
2024-11-12 18:38:22 -0600

    Update App.js
    Signed-off-by: Dev713 <53387865+erikg713@users.noreply.github.com>

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,35 +1,31 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import './App.css';
+
+// Import pages and components
 import Login from './components/Login';
 import Register from './components/Register';
 import ProductList from './components/ProductList';
-
-function App() {
-    return (
-        <Router>
-            <div>
-                <h1>Palace of Goods</h1>
-                <Routes>
-                    <Route path="/login" element={<Login />} />
-                    <Route path="/register" element={<Register />} />
-                    <Route path="/products" element={<ProductList />} />
-                </Routes>
-            </div>
-        </Router>
-    );
-}
-
-export default App;
-
-import React from 'react';
 import Marketplace from './components/Marketplace';
 
-function App() {
-  return (
-    <div className="App">
-      <Marketplace />
+const App = () => (
+  <Router>
+    <div>
+      <h1>Palace of Goods</h1>
+      {/* Toast Notifications */}
+      <ToastContainer />
+
+      {/* Routes and Components */}
+      <Routes>
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/products" element={<ProductList />} />
+        <Route path="/marketplace" element={<Marketplace />} />
+      </Routes>
     </div>
-  );
-}
+  </Router>
+);
 
 export default App;
