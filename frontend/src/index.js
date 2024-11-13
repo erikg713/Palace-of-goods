0146d541e2279e8aa16c6f9a77155b1d8a80293f
Dev713 <53387865+erikg713@users.noreply.github.com>
2024-11-12 18:43:57 -0600

    Update index.js
    Signed-off-by: Dev713 <53387865+erikg713@users.noreply.github.com>

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import { AuthProvider } from './context/AuthContext';
 
 ReactDOM.render(
-    <React.StrictMode>
-        <App />
-    </React.StrictMode>,
-    document.getElementById('root')
+  <AuthProvider>
+    <App />
+  </AuthProvider>,
+  document.getElementById('root')
 );
