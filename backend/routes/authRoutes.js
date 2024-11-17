c09f95b40f9d667e348aed7a3449de1c115c853b
Dev713 <53387865+erikg713@users.noreply.github.com>
2024-11-17 03:44:09 -0600

    Update authRoutes.js
    Signed-off-by: Dev713 <53387865+erikg713@users.noreply.github.com>

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -1,1 +1,8 @@
+import express from 'express';
+import { register, login } from '../controllers/authController';
+const router = express.Router();
 
+router.post('/register', register);
+router.post('/login', login);
+
+export default router;
