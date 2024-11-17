9dd9b6077d9216d989282b46f738a6edcffb4b32
Dev713 <53387865+erikg713@users.noreply.github.com>
2024-11-17 11:31:35 -0600

    Update Footer.js
    Signed-off-by: Dev713 <53387865+erikg713@users.noreply.github.com>

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -1,1 +1,9 @@
+import React from 'react';
 
+const Footer = () => (
+  <footer className="bg-light text-center mt-4">
+    <p>© 2024 Palace of Goods. All Rights Reserved.</p>
+  </footer>
+);
+
+export default Footer;
