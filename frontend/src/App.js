5f2c93441f3af89518cbf353bbf96dfb6d1abbc9
Dev713 <53387865+erikg713@users.noreply.github.com>
2024-11-17 03:40:55 -0600

    Update App.js
    Signed-off-by: Dev713 <53387865+erikg713@users.noreply.github.com>

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -43,3 +43,23 @@ function App() {
 }
 
 export default App;
+// frontend/src/App.js
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Navbar from './components/Navbar';
+import HomePage from './pages/HomePage';
+import PaymentPage from './pages/PaymentPage';
+
+function App() {
+  return (
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/payment" element={<PaymentPage />} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
