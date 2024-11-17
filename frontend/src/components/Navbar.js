609638542e53ef036074b57bec9fcf2e7650aaf3
Dev713 <53387865+erikg713@users.noreply.github.com>
2024-11-17 11:31:04 -0600

    Update Navbar.js
    Signed-off-by: Dev713 <53387865+erikg713@users.noreply.github.com>

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -13,3 +13,24 @@ const Navbar = () => (
 );
 
 export default Navbar;
+
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const Navbar = () => (
+  <nav className="navbar navbar-expand-lg navbar-light bg-light">
+    <div className="container-fluid">
+      <Link className="navbar-brand" to="/">Palace of Goods</Link>
+      <div className="collapse navbar-collapse">
+        <ul className="navbar-nav me-auto">
+          <li className="nav-item"><Link className="nav-link" to="/">Home</Link></li>
+          <li className="nav-item"><Link className="nav-link" to="/marketplace">Marketplace</Link></li>
+          <li className="nav-item"><Link className="nav-link" to="/profile">Profile</Link></li>
+          <li className="nav-item"><Link className="nav-link" to="/login">Login</Link></li>
+        </ul>
+      </div>
+    </div>
+  </nav>
+);
+
+export default Navbar;
