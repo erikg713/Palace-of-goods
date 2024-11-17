c7ed6f870241eef3e4dee942023d62775597982e
Dev713 <53387865+erikg713@users.noreply.github.com>
2024-11-17 11:43:24 -0600

    Update index.js
    Signed-off-by: Dev713 <53387865+erikg713@users.noreply.github.com>

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,4 +1,5 @@
-import 'bootstrap/dist/css/bootstrap.min.css';
+// In index.js
+import 'bootstrap/dist/css/bootstrap.min.css';import 'bootstrap/dist/css/bootstrap.min.css';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
