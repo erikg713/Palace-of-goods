e4fceaa4951d5ab01993fa17cd4ad03718307c9d
Dev713 <53387865+erikg713@users.noreply.github.com>
2024-11-17 11:33:00 -0600

    Update Login.js
    Signed-off-by: Dev713 <53387865+erikg713@users.noreply.github.com>

--- frontend/src/pages/Login.js
+++ frontend/src/pages/Login.js
@@ -1,1 +1,20 @@
+import React from 'react';
 
+const Login = () => (
+  <div>
+    <h1>Login</h1>
+    <form>
+      <div className="mb-3">
+        <label>Email</label>
+        <input type="email" className="form-control" />
+      </div>
+      <div className="mb-3">
+        <label>Password</label>
+        <input type="password" className="form-control" />
+      </div>
+      <button className="btn btn-primary">Login</button>
+    </form>
+  </div>
+);
+
+export default Login;
