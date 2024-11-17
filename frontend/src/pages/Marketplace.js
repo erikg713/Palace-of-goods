2ed57f3078ed207fd13ea942f60eb4eca055e34f
Dev713 <53387865+erikg713@users.noreply.github.com>
2024-11-17 11:32:38 -0600

    Update Marketplace.js
    Signed-off-by: Dev713 <53387865+erikg713@users.noreply.github.com>

--- frontend/src/pages/Marketplace.js
+++ frontend/src/pages/Marketplace.js
@@ -1,1 +1,10 @@
+import React from 'react';
 
+const Marketplace = () => (
+  <div>
+    <h1>Marketplace</h1>
+    <p>Browse and purchase exclusive Web3 items!</p>
+  </div>
+);
+
+export default Marketplace;
