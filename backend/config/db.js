45aa161e8885ec57b0b45c07b1bae3c29d726327
Dev713 <53387865+erikg713@users.noreply.github.com>
2024-11-17 03:19:23 -0600

    Update db.js
    Signed-off-by: Dev713 <53387865+erikg713@users.noreply.github.com>

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,1 +1,13 @@
+// backend/config/db.js
+import { Pool } from 'pg';
+import dotenv from 'dotenv';
 
+dotenv.config();
+
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+});
+
+export default {
+  query: (text, params) => pool.query(text, params),
+};
