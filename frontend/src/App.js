ff77a7e716e1a8b639248405d8b837990f42554b
Dev713 <53387865+erikg713@users.noreply.github.com>
2024-11-17 22:32:57 -0800

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,124 +1,42 @@
 import React from 'react';
-import { BrowserRouter as Router } from 'react-router-dom';
-import Navbar from './components/Navbar';
-import Footer from './components/Footer';
-import AppRoutes from './routes';
-import { ToastContainer } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
-import './App.css';
-
-const App = () => (
-  <Router>
-    <div>
-      <Navbar />
-      <ToastContainer />
-      <AppRoutes />
-      <Footer />
-    </div>
-  </Router>
-);
-
-export default App;
-import React from 'react';
-import { BrowserRouter as Router } from 'react-router-dom';
-import Navbar from './components/Navbar';
-import Footer from './components/Footer';
-import AppRoutes from './routes';
-import { ToastContainer } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
-import './App.css';
-
-const App = () => (
-  <Router>
-    <div>
-      <Navbar />
-      <ToastContainer />
-      <AppRoutes />
-      <Footer />
-    </div>
-  </Router>
-);
-
-export default App;import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Navbar from './components/Navbar';
+import Footer from './components/Footer';
 import { ToastContainer } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
-import './App.css';
+import 'react-toastify/dist/ReactToastify.css'; // Toastify styles
+import './App.css'; // Custom styles
 
 // Import pages and components
 import Login from './components/Login';
 import Register from './components/Register';
 import ProductList from './components/ProductList';
 import Marketplace from './components/Marketplace';
+import HomePage from './pages/HomePage';
+import PaymentPage from './pages/PaymentPage';
 
 const App = () => (
   <Router>
-    <div>
-      <h1>Palace of Goods</h1>
-      {/* Toast Notifications */}
+    {/* Navbar */}
+    <Navbar />
+
+    {/* Main content */}
+    <div className="container mt-4">
+      {/* Toast notifications */}
       <ToastContainer />
 
-      {/* Routes and Components */}
+      {/* Routes */}
       <Routes>
+        <Route path="/" element={<HomePage />} />
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
         <Route path="/products" element={<ProductList />} />
         <Route path="/marketplace" element={<Marketplace />} />
-      </Routes>
-    </div>
-  </Router>
-);
-
-export default App;
-// frontend/src/App.js
-import React from 'react';
-import PaymentForm from './components/PaymentForm';
-
-function App() {
-  return (
-    <div>
-      <h1>Palace of Goods - Payment Portal</h1>
-      <PaymentForm />
-    </div>
-  );
-}
-
-export default App;
-// frontend/src/App.js
-import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Navbar from './components/Navbar';
-import HomePage from './pages/HomePage';
-import PaymentPage from './pages/PaymentPage';
-
-function App() {
-  return (
-    <Router>
-      <Navbar />
-      <Routes>
-        <Route path="/" element={<HomePage />} />
         <Route path="/payment" element={<PaymentPage />} />
       </Routes>
-    </Router>
-  );
-}
-
-export default App;
-import React from 'react';
-import { BrowserRouter as Router } from 'react-router-dom';
-import RoutesList from './routes';
-import Navbar from './components/Navbar';
-import Footer from './components/Footer';
-import { ToastContainer } from 'react-toastify';
-
-const App = () => (
-  <Router>
-    <Navbar />
-    <div className="container mt-4">
-      <RoutesList />
     </div>
+
+    {/* Footer */}
     <Footer />
-    <ToastContainer />
   </Router>
 );
 
